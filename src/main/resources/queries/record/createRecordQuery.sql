3eb098e69488969a10e951873e5da56395acaf97
andrew <andrii.skrypnik@gmail.com>
2020-03-29 22:00:04 +0300

    Fix bug when record created

--- src/main/resources/queries/record/createRecordQuery.sql
+++ src/main/resources/queries/record/createRecordQuery.sql
@@ -1,3 +1,3 @@
 INSERT INTO record (bar_code, release_bar_code, check_num, supplier_edrpou, purchase_date, purchase_price, sell_price,
                     state, state_check_date)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
